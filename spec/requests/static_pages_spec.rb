cbe899c334f037dc7d140421c24cae93c009835c
Stephen Bussey <sb8244@cs.ship.edu>
2013-12-26 00:28:05 -0500

    Add base title to rspec static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App | ' }
+
   describe "Home Page" do
   	it "should have 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title}Home")
     end
   end
 
@@ -21,7 +24,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title}Help")
     end
   end
 
@@ -33,7 +36,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title}About")
     end
   end
 end
