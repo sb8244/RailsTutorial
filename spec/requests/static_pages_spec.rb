4557f9b2d0701291436837f9d56aac9fa32bd261
Stephen Bussey <sb8244@cs.ship.edu>
2014-01-11 12:39:54 -0500

    Initial commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-  let(:base_title) { 'Ruby on Rails Tutorial Sample App | ' }
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe "Home Page" do
   	it "should have 'Sample App'" do
@@ -12,7 +12,12 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -24,7 +29,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title}Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +41,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title}About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 end
