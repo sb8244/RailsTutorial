3144148014c2a0f73edec2108d1c4be429f615c9
Stephen Bussey <sb8244@cs.ship.edu>
2014-01-11 13:56:19 -0500

    Clean up layout, routes, and new contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,42 +6,55 @@ describe "StaticPages" do
 
   describe "Home Page" do
   	it "should have 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title("| Home")
     end
   end
 
   describe "Help page" do
     it "should have 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
+
+  describe "Contact page" do
+    it "should have 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end
