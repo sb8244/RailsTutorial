fbb5e688ce09c61353708d1806d493e3d5a99a6f
Stephen Bussey <sb8244@cs.ship.edu>
2014-01-12 15:32:50 -0500

    Form works but needs styling

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,30 @@ describe "User Pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@Example.com"
+        fill_in "Password",     with: "password"
+        fill_in "Confirmation", with: "password"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count)
+      end
+    end
+  end
+
 end
